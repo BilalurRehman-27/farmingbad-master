ac980be07ff9c39cd816254b31c33c6d34140b45
Bilal <Bilal_ur_Rehman@outlook.com>
2020-11-13 14:24:50 +0500

    Added className on Navbar

--- src/components/TopBar/components/Nav.tsx
+++ src/components/TopBar/components/Nav.tsx
@@ -8,22 +8,37 @@ import Nft from '../../../assets/images/Nft.png'
 
 const Nav: React.FC = () => {
   return (
-    <StyledNav>
-      <StyledLink exact activeClassName="active" to="/">
-        <HomeImage src={Home}></HomeImage>
-      </StyledLink>
-      <StyledLink exact to="/rvlLab">
-        <RVImage src={RVLab}></RVImage>
-      </StyledLink>
-      <StyledLink exact to="/farms1">
-        <SuperImage src={SuperLab}></SuperImage>
-      </StyledLink>
-      <StyledLink exact to="/farms2">
-        <NftImage src={Nft}></NftImage>
-      </StyledLink>
-    </StyledNav>
+    <MenuWrap className="menu-wrap">
+      <StyledNav>
+        <StyledLink exact activeClassName="active" to="/">
+          <HomeImage src={Home}></HomeImage>
+        </StyledLink>
+        <StyledLink exact to="/farms">
+          <RVImage src={RVLab}></RVImage>
+        </StyledLink>
+        <StyledLink exact to="/farms1">
+          <SuperImage src={SuperLab}></SuperImage>
+        </StyledLink>
+        <StyledLink exact to="/farms2">
+          <NftImage src={Nft}></NftImage>
+        </StyledLink>
+      </StyledNav>
+    </MenuWrap>
   )
 }
+
+const MenuWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 50px;
+  @media (max-width: 767px) {
+    &.menu-wrap {
+      display: flex !important;
+      justify-content: space-between !important;
+      padding: 0 10px !important;
+    }
+  }
+`
 
 const StyledNav = styled.nav`
   align-items: center;
@@ -48,19 +63,19 @@ const NftImage = styled.img`
 `
 
 const StyledLink = styled(NavLink)`
-  color: ${props => props.theme.color.grey[400]};
-  padding-left: ${props => props.theme.spacing[3]}px;
-  padding-right: ${props => props.theme.spacing[3]}px;
+  color: ${(props) => props.theme.color.grey[400]};
+  padding-left: ${(props) => props.theme.spacing[3]}px;
+  padding-right: ${(props) => props.theme.spacing[3]}px;
   text-decoration: none;
   &:hover {
-    color: ${props => props.theme.color.grey[500]};
+    color: ${(props) => props.theme.color.grey[500]};
   }
   &.active {
-    color: ${props => props.theme.color.primary.main};
+    color: ${(props) => props.theme.color.primary.main};
   }
   @media (max-width: 400px) {
-    padding-left: ${props => props.theme.spacing[2]}px;
-    padding-right: ${props => props.theme.spacing[2]}px;
+    padding-left: ${(props) => props.theme.spacing[2]}px;
+    padding-right: ${(props) => props.theme.spacing[2]}px;
   }
 `
 
